87cd968d9dd79e5549915b17c4da0c32513dc7f6
Dikshant Dak <76099944+dikshant-dak@users.noreply.github.com>
2023-03-01 21:21:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,11 @@
-import React, { createContext, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/header/Header';
-import Home from './pages/Home/Home';
-import Images from './components/images/Images';
-import useAxios from './hooks/useAxios';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.scss";
 
-
-// Create Context
-export const ImageContext = createContext();
-
-function App() {
-    const [searchImage, setSearchImage] = useState("");
-
-    const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cars&client_id=ivaIgztV8crikZK3w1mvxQgxMJMsF9ZxJOfrbPlG_Hk`);
-
-    // console.log(response);
-
-    const value = {
-        response,
-        isLoading,
-        error,
-        fetchData,
-        searchImage,
-        setSearchImage
-    }
-
-
-    return (
-        <BrowserRouter>
-            <ImageContext.Provider value={value}>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-                <Images />
-            </ImageContext.Provider>
-        </BrowserRouter>
-    )
-}
-
-export default App;
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
